6df67c1a0af6dcced24ab0ce3f05c009b4d08263
Ali Hadi <ah1556499@gmail.com>
2022-09-19 02:25:23 +0500

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,8 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
-
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
 
 @Options({
   components: {
